b02a59f7548e4ed2ab4d52fd2a64f7400e0c2928
Arthur <arthur-1702@hotmail.com>
2025-10-07 18:50:38 -0300

    Iniciar repo do Devops

---
+++ Application/Interfaces/IGameService.cs
@@ -0,0 +1,14 @@
+﻿using Application.DTO.Game;
+
+namespace Application.Interfaces
+{
+    public interface IGameService
+    {
+        Task<IEnumerable<GameResponse>> GetAllGamesAsync();
+        GameResponse GetGameById(int id);
+        GameSearchResponse SearchGames(GameSearchRequest request, int page = 0, int pageSize = 10);
+        GameResponse AddGame(GameRequest game);
+        GameResponse UpdateGame(GameRequest game);
+        bool DeleteGame(int id);
+    }
+}
